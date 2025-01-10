cd00bb7a286f328ebf899d89008f4dc45147dca3
choseongchan3712 <choseongchan3712@naver.com>
2025-01-10 11:01:08 +0900

    section01 part1 complete

---
+++ src/section01/part1.java
@@ -0,0 +1,21 @@
+package section01;
+
+public class part1 {
+    public static void main(String[] args) {
+        // 실행문은 변수의 선언 및 변수값 저장, 메서드 호출에 해당하는 코드를 말함. 실행문 끝에는 반드시 (;)을 붙여서 끝났음을 표시해야함.
+
+        // 한 줄에 여러개의 실행문 작성
+        int a; int b; a = 1; b = 2;
+
+        // 한줄에 하나의 실행문 작성
+        a = 1;
+        b = 2;
+
+        // 하나의 실행문을 여러줄에 작성
+        a =
+        1;
+        b =
+        2;
+
+    }
+}
